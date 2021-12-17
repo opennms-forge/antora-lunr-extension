ce3d6db0d2b00ae8ed0ee35c003c30455f3284e2
Dan Allen <dan@opendevise.com>
2021-12-17 02:38:26 -0700

    remove redundant consts in integration test

--- test/integration-test.js
+++ test/integration-test.js
@@ -20,8 +20,6 @@ describe('generateSite()', () => {
   after(() => fsp.rm(WORK_DIR, { recursive: true, force: true }))
 
   it('should generate a site with a search index', async () => {
-    const playbookFile = ospath.join(FIXTURES_DIR, 'docs-site', 'antora-playbook.yml')
-    const cacheDir = ospath.join(WORK_DIR, '.cache', 'antora')
     await generateSite(['--playbook', playbookFile, '--to-dir', outputDir, '--cache-dir', cacheDir, '--quiet'], {
       DOCSEARCH_ENABLED: 'true',
     })
@@ -34,7 +32,6 @@ describe('generateSite()', () => {
       },
     }
     require(ospath.join(outputDir, 'search-index.js'))
-
     const startPageContents = await fsp.readFile(ospath.join(outputDir, 'antora-lunr', 'index.html'))
     const $ = cheerio.load(startPageContents)
     expect($('#search-input')).to.have.lengthOf(1)
