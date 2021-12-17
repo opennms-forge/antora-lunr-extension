e6ba4abd39bd95eb8dfe2c7840a7e4a64800289d
Dan Allen <dan@opendevise.com>
2021-12-16 18:29:48 -0700

    don't leak global.window in integration test

--- test/integration-test.js
+++ test/integration-test.js
@@ -25,17 +25,20 @@ describe('generateSite()', () => {
     await generateSite(['--playbook', playbookFile, '--to-dir', outputDir, '--cache-dir', cacheDir, '--quiet'], {
       DOCSEARCH_ENABLED: 'true',
     })
-    global.window = {}
-    global.window.antoraLunr = {}
-    global.window.antoraLunr.init = (index) => {
-      expect(index.store['/antora-lunr/index.html'].title).to.equal('Antora x Lunr')
-      expect(index.store['/antora-lunr/index.html'].url).to.equal('/antora-lunr/index.html')
+    global.window = {
+      antoraLunr: {
+        init (index) {
+          expect(index.store['/antora-lunr/index.html'].title).to.equal('Antora x Lunr')
+          expect(index.store['/antora-lunr/index.html'].url).to.equal('/antora-lunr/index.html')
+        },
+      },
     }
     require(ospath.join(outputDir, 'search-index.js'))
 
     const startPageContents = await fsp.readFile(ospath.join(outputDir, 'antora-lunr', 'index.html'))
     const $ = cheerio.load(startPageContents)
     expect($('#search-input')).to.have.lengthOf(1)
+    delete global.window
   })
 
   it('should output lunr.js client/engine to js vendor directory of UI output folder', async () => {
